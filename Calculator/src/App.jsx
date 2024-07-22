0ab1f3898374aa14cc034e61a6a1f87f0cc4fc87
shubham.landge <shubhamlandge657@gmail.com>
2024-07-22 12:56:55 +0530

    Updated V1.0.0

--- Calculator/src/App.jsx
+++ Calculator/src/App.jsx
@@ -4,34 +4,51 @@ import UserInput from "./Component/UserInput";
 import ItemButtons from "./Component/ItemButtons";
 import { useState } from "react";
 const App = () => {
-  const itembuttons= ['%','C','DEL','/',
-    '7','8','9','*','4','5','6','-','1','2','3','+','0','.','='];
+  const itembuttons = [
+    "C",
+    "DEL",
+    "%",
+    "7",
+    "8",
+    "/",
+    "9",
+    "4",
+    "*",
+    "5",
+    "6",
+    "-",
+    "1",
+    "2",
+    "+",
+    "3",
+    "0",
+    ".",
+    "=",
+  ];
 
-  const [userInput,setUserInput] = useState('');
+  const [userInput, setUserInput] = useState("");
 
   const handleInput = (buttontext) => {
-    if (buttontext === 'C'){
-      setUserInput('')
-    }else if(buttontext === '='){
-      const result =eval(userInput)
+    if (buttontext === "C") {
+      setUserInput("");
+    } else if (buttontext === "=") {
+      const result = eval(userInput);
       setUserInput(result.toString());
-    }else if(buttontext === 'DEL') {
-      console.log(userInput,'current')
+    } else if (buttontext === "DEL") {
+      console.log(userInput, "current");
       setUserInput(userInput.slice(0, -1));
-    }else{
+    } else {
       const newtext = userInput + buttontext;
-      setUserInput(newtext)
+      setUserInput(newtext);
     }
-  }
+  };
   return (
     <>
       <center className="kg-center">
         <div className="kg-container">
-          <div className="inner-container">
           <Heading />
           <UserInput userInput={userInput} />
-          <ItemButtons itembuttons={itembuttons} onClickInput={handleInput}/>
-          </div>
+          <ItemButtons itembuttons={itembuttons} onClickInput={handleInput} />
         </div>
       </center>
     </>
--- Calculator/src/Component/ItemButtons.jsx
+++ Calculator/src/Component/ItemButtons.jsx
@@ -2,13 +2,13 @@ import styles from "./ItemButtons.module.css";
 
 const ItemButtons = ({ itembuttons,onClickInput }) => {
     return (
-      <div className={styles.bContainer}>
+      <>
         {itembuttons.map((btn, index) => (
-          <button type="submit" className={`${styles.buttons} ${btn ==='=' && styles.primary} ${btn ==='DEL' && styles.danger}`} key={index} onClick={() => onClickInput(btn)}>
+          <button type="submit" className={`${['C', '+', '-','DEL','/','*','%','.','='].includes(btn) ? styles.icons : styles.buttons}`} key={index} onClick={() => onClickInput(btn)}>
             {btn}
           </button>
         ))}
-      </div>
+      </>
     );
   };
 
